7292cab710b2518eebfee9056b1042693ad85382
Khurelbaatar Anar <tactoj@gmail.com>
2016-06-13 21:15:38 +0200

    update

---
+++ sites/onzetlenul-alapitvany/js/function.js
@@ -0,0 +1,45 @@
+(function($){
+
+	var $navToggleBtn = $('.nav--toggle'),
+			$body,
+			$window,
+			$settings = {
+				scrollDuration: 300
+			};
+
+	function onClick () {
+		var $this = $(this),
+				$menu = $('.nav ul');
+
+		$this.toggleClass('close');
+		$menu.toggleClass('open');
+	}
+
+	function linkClick(e) {
+		var $this = $(this),
+				href = $this.attr('href'),
+				$target = $(href);
+
+		if ($target.length == 0)
+			return;
+
+		e.preventDefault();
+
+		$body.animate({
+				scrollTop: $target.offset().top
+			}, $settings.duration);
+		}
+
+		function bindings () {
+			$navToggleBtn.on('click', onClick);
+			$('a[href^="#"]').on('click', linkClick);
+		}
+
+	$(document).ready(function(){
+		$body = $('html, body');
+		$window = $(window);
+
+		bindings();
+	});
+
+})(jQuery);
